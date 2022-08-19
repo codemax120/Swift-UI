441dc21a526997233a84f16cdf0d7ba9c73f8d04
Max Herrera <harvey.herrera@lumation.co>
2022-08-19 12:45:10 -0600

    Initial Commit

---
+++ Practice Swift UI/Practice_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Swift_UIApp.swift
+//  Practice Swift UI
+//
+//  Created by Max Herrera on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
